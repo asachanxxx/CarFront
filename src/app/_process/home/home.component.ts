589831d5b65ec36471a73946dd8060b04fb130a9
Asanga Chan <cspl.itm@googlemail.com>
2019-01-23 06:55:05 +0530

    change alot

--- src/app/_process/home/home.component.ts
+++ src/app/_process/home/home.component.ts
@@ -4,6 +4,7 @@ import { ScriptLoaderService } from '../../_services/script-loader.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit, AfterViewInit {
 
